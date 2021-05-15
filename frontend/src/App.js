6b8930e9894c8d3552ee64ca7d2198abee1d57ec
r00tshaim <shaim456@gmail.com>
2021-05-15 20:56:14 +0530

    adding HomeScreen Component to list products
    Changes Include:
    
    -Adding HomeScreen component
    
    -Adding Product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import HomeScreen from './components/HomeScreen'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
