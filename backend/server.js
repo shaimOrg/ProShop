63bd4dedd0ff17305ac67c9e5ef10886ff1629d8
r00tshaim <shaim456@gmail.com>
2021-06-05 10:13:46 +0530

    creating enviromental variable file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const products = require('./data/products')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 app.get('/', (req, res) => {
     res.send('API is running')
@@ -15,4 +18,12 @@ app.get('/api/products/:id', (req, res) => {
     res.send(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+getEnv = () => {
+    if (process.env.NODE_ENV)
+        return process.env.NODE_ENV
+    return "PRODUCTION"
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${getEnv()} mode on port ${PORT}`))
