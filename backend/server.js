ea6fe8f65ec11f5ada11a3542e83d22001a5dcb1
r00tshaim <shaim456@gmail.com>
2021-06-26 14:57:15 +0530

    Issue: #43
    Customer Error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -15,6 +16,9 @@ app.use('/api/products', productRoutes)
 app.get('/', (req, res) => {
     res.send('API is running')
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 const getEnv = () => {
     if (process.env.NODE_ENV)
